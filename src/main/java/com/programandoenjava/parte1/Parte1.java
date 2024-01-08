d4b26d53f63822be96d3b4a510ea6c5e3c39454a
Andres <andrespodadera87@gmail.com>
2024-01-08 21:39:12 +0100

    parte 1 y 2 completadas

--- src/main/java/com/programandoenjava/parte1/Parte1.java
+++ src/main/java/com/programandoenjava/parte1/Parte1.java
@@ -1,15 +1,43 @@
 package com.programandoenjava.parte1;
 
 public class Parte1 {
-
     /**
-     * Escribe un programa en Java que reciba una cadena de texto y devuelva la
-     * misma cadena pero con cada palabra invertida. Por ejemplo, para la entrada "Hola Mundo",
-     * la salida debería ser "aloH odluM".
+     * Programa que invierte una cadena de texto.
+     *
      * @param cadena cadena de entrada
      * @return cadena invertida
      */
-    public String invertirPalabras(final String cadena) {;
-        return null;
+    public String invertirPalabras(final String cadena) {
+        if (cadena == null) {
+            return "";
+        }
+        //StringBuilder es más eficiente que String para concatenar cadenas
+        StringBuilder fraseInvertida = new StringBuilder();
+        //Recorremos la frase
+        for (int i = 0; i < cadena.length(); i++) {
+            StringBuilder palabraInvertida = new StringBuilder();
+            //Si el caracter no es una letra
+            if (!Character.isLetter(cadena.charAt(i))) {
+                //Lo añadimos a la frase invertida
+                fraseInvertida.append(cadena.charAt(i));
+            } else {
+               //Si el caracter es una letra, recorremos la palabra hasta el final
+                for (int j = i; j < cadena.length() && cadena.charAt(j) != ' '; j++) {
+                    //Si el caracter es una letra, lo añadimos al principio de la palabra invertida
+                    if (Character.isLetter(cadena.charAt(j))) {
+                        palabraInvertida.insert(0, cadena.charAt(j));
+                    }else {
+                        //Si el caracter es un espacio en blanco, lo añadimos a la palabra invertida
+                        palabraInvertida.append(cadena.charAt(j));
+                    }
+                }
+                //Añadimos la palabra invertida a la frase invertida
+                fraseInvertida.append(palabraInvertida);
+                //Movemos el índice hasta el final de la palabra
+                i += palabraInvertida.length() - 1;
+            }
+        }
+        //toString() convierte el StringBuilder en String
+        return fraseInvertida.toString();
     }
 }
--- src/main/java/com/programandoenjava/parte2/Parte2.java
+++ src/main/java/com/programandoenjava/parte2/Parte2.java
@@ -1,26 +1,34 @@
 package com.programandoenjava.parte2;
 
+import java.util.ArrayList;
+
 /**
- * Implementa en Java que tenga un método
- * para agregar números enteros y otro método para calcular
- * la suma de todos los elementos en la lista.
+ * Programa que utiliza un ArrayList para almacenar números doubles(cualquier número)
+ * Permite agregar números enteros a la lista y calcular la suma de todos los números.
  */
 public class Parte2 {
+    /**
+     * @listaNumeros Lista de números reales.
+     */
+    ArrayList<Double> listaNumeros = new ArrayList<>();
 
     /**
      * Calcula la suma de todos los números enteros.
      *
      * @return La suma de los números enteros.
      */
-    public int sumarNumeros() {
-        return 0;
+    public double sumarNumeros() {
+        //Programacion funcional, reduce suma todos los elementos de la lista
+       return listaNumeros.stream().reduce(0.0, Double::sum);
     }
 
     /**
-     * Agrega un número entero a la lista.
+     * Agrega un número a la lista.
      *
-     * @param numero El número entero que se va a agregar.
+     * @param numero El número que se va a agregar.
      */
-    public void agregarNumero(int numero) {
+    public void agregarNumero(double numero) {
+        //Agrega un numero a la lista
+        listaNumeros.add(numero);
     }
 }
